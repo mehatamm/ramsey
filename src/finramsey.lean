04be3a854260cae6fa724dce6378026e748d3b5d
Mattias Ehatamm <mati@ehatamm.com>
2023-02-16 12:30:32 -0500

    procession proved

--- src/finramsey.lean
+++ src/finramsey.lean
@@ -87,8 +87,6 @@ end
 
 @[reducible] def edges_from_in_finset (n : ℕ) (v : fin n) (Y : finset (E n)) : set (E n):= {e ∈ Y | e 0 = v}
 
---@[reducible] def edges_from2 (n : ℕ) (v : fin n): Type := {e : E n // e 0 = v} --define as subtype to make pigeonhole easier
-
 
 def order_embedding_equiv_finset {α : Type*} [linear_order α] (k : ℕ) : 
   {s : finset α // s.card = k} ≃ (fin k ↪o α) := 
@@ -111,38 +109,58 @@ def order_embedding_equiv_finset {α : Type*} [linear_order α] (k : ℕ) :
 
 @[reducible] def edge : Type:= fin 2 ↪o ℕ
 
+def edge_of {i j : ℕ} (h : i < j) : edge:= --disgusting, do not use definitionally
+{ to_fun := λ x, if xe0: x = 0 then i else j,
+  inj' := begin
+    intros x y xyeq, cases mem_fin_2 x, {
+      cases mem_fin_2 y, 
+      {
+        simp[h_1, h_2],
+      },
+      simp only [h_1, h_2, eq_self_iff_true, dite_eq_ite, if_true, fin.one_eq_zero_iff, nat.succ_succ_ne_one, if_false] at xyeq,
+      apply absurd xyeq (ne_of_lt h),
+    },
+    cases mem_fin_2 y,
+    {
+      simp only [h_1, h_2, eq_self_iff_true, dite_eq_ite, if_true, fin.one_eq_zero_iff, nat.succ_succ_ne_one, if_false] at xyeq,
+      apply absurd xyeq.symm (ne_of_lt h),
+    },
+    simp [h_1, h_2],
+  end,
+  map_rel_iff' := begin
+    intros x y, cases mem_fin_2 x,{
+      cases mem_fin_2 y, 
+      {
+        simp [h_1, h_2],
+      },
+      simp [h_1, h_2, h, le_of_lt], 
+    },
+    cases mem_fin_2 y,
+    {
+      simp [h_1, h_2, h, le_of_lt],
+    },
+    simp [h_1, h_2],
+  end}
 
--- lemma edges_from_card {n k : ℕ} {v : fin n}(hvk : n = v + 1 + k)  : fintype.card (edges_from n v) = k :=
--- begin
+theorem edge_of_0 {i j : ℕ} (h : i < j) : (edge_of h) 0 = i:=
+begin
+  rw edge_of, simp,
+end
 
---   unfold edges_from, 
-  
---   convert (@fintype.card_of_bijective (fin k) _ _ _ _ _).symm,
---   have eq: fin k ≃ edges_from n v:=
---   {
---     to_fun:=begin
---       rintros ⟨i, lk⟩, have vltn: ((v:ℕ)+i+1) < n, linarith, have vlvi: (v:ℕ) < v+ i + 1,
---       linarith, have vlvif: v < ⟨v+i+1, vltn⟩, apply vlvi, refine ⟨edge vlvif, _⟩, dsimp, refl, 
---     end,
---     inv_fun:=begin
---       rintros ⟨e, e0⟩, have le12: (0:fin 2) ≤ (1:fin 2):= by norm_num, have e01:=e.map_rel_iff.2 le12, 
---       have: e 0 = v, apply e0, rw this at e01, use e 1 - v, sorry,
---     end,
---     left_inv:=sorry,
---     right_inv:=sorry,
---   }
--- end
-
+theorem edge_of_1 {i j : ℕ} (h : i < j) : (edge_of h) 1 = j:=
+begin
+  rw edge_of, simp,
+end
 
 instance edges_from_finset_fin (Y: finset ℕ) (v : ℕ) : fintype {e : edge | e 0 = v ∧ e 1 ∈ Y} :=
 begin
   set f : {e : edge | e 0 = v ∧ e 1 ∈ Y} → Y := λ e, ⟨e.1 1, e.2.2⟩ with hfdef, 
   have hf : f.injective, 
-  { rintros ⟨x,⟨hx,hx'⟩⟩ ⟨y,⟨hy,hy'⟩⟩ hxy, simp [hfdef] at hxy, ext a,  sorry, },
+  { rintros ⟨x,⟨hx,hx'⟩⟩ ⟨y,⟨hy,hy'⟩⟩ hxy, simp [hfdef] at hxy, ext a, cases mem_fin_2 a, rw h, simp [hx, hy], rw h, simp [hxy], },
   apply fintype.of_injective f hf,
 end 
 
-def edges_from_finset (Y: finset ℕ) (v : ℕ) : finset edge := 
+@[reducible] def edges_from_finset (Y: finset ℕ) (v : ℕ) : finset edge := 
   set.to_finset {e : edge | e 0 = v ∧ e 1 ∈ Y}
 
 -- example {α : Type} (f : α → β) (hf : f.injective) (s : finset β) (h : ∀ a, f a ∈ s) : finite α :=
@@ -150,20 +168,26 @@ def edges_from_finset (Y: finset ℕ) (v : ℕ) : finset edge :=
   
 -- end  
 
-lemma edges_from_finset_card (Y: finset ℕ) {v k : ℕ} (hvy : ∀ y ∈ Y, v ≤ y) (yc: k+1 ≤ Y.card):
-k ≤ (edges_from_finset Y v).card:=
+lemma edges_from_finset_card (Y: finset ℕ) {v k : ℕ} (hvy : ∀ y ∈ (Y.erase v), v < y):
+(Y.erase v).card ≤ (edges_from_finset Y v).card:=
 begin
-  -- set f : ℕ → edge := λ e, e 1,   
-  have := @finset.card_le_card_of_inj_on _ _ (edges_from_finset Y v) (Y.erase v) f, 
+  set f : ℕ → edge := λ x, if h: x ∈ (Y.erase v) then edge_of (hvy x h) else edge_of (nat.lt_succ_self x) with f_def,
+  apply @finset.card_le_card_of_inj_on _ _ (Y.erase v) (edges_from_finset Y v) f,
+  {
+    intros a aye, rw f_def, simp [aye],
+    have e0:= edge_of_0 (hvy a aye), have e1:= edge_of_1 (hvy a aye), simp [e0, e1, aye], exact finset.mem_of_mem_erase aye,
+  },
+  intros a aye b bye fabeq, rw f_def at fabeq,
+  simp [aye, bye] at fabeq, have a1:= edge_of_1 (hvy a aye), have b1:= edge_of_1 (hvy b bye),
+  rw fabeq at a1, rw b1 at a1, exact a1.symm, 
 end 
 
 def has_fav (f : edge → fin 2) (Y : finset ℕ):= 
 ∀ y ∈ Y, ∃ c : fin 2,
 ∀ e ∈ (edges_from_finset Y y), f e = c
 
-
 lemma favcolor_fixed (k : ℕ): 
-∃ n₀: ℕ, ∀ X : finset ℕ, X.card ≥ n₀ → ∀ f: edge → fin 2, ∃ Y ⊆ X,
+∃ n₀: ℕ, ∀ X : finset ℕ, n₀ ≤ X.card  → ∀ f: edge → fin 2, ∃ Y ⊆ X,
 Y.card = k ∧ has_fav f Y :=
 begin
   induction k with k ih,
@@ -175,7 +199,59 @@ begin
   have xnone: X.nonempty, apply finset.card_pos.1, linarith,
   set xmin:= X.min' xnone,
   set xfmin:= edges_from_finset X xmin, 
-
+  have xfc: (X.erase xmin).card ≤ xfmin.card, apply edges_from_finset_card X (λ y yxe, finset.min'_lt_of_mem_erase_min' X xnone yxe),
+  exact xmin, rw (finset.card_erase_of_mem (finset.min'_mem X xnone)) at xfc,
+  have xfcn: 2*n₀ ≤ xfmin.card, linarith [finset.card_pos.2 xnone], --finally fully proved cardinality!
+  have pigeonhole: ∃ friends : (finset edge), friends ⊆ xfmin ∧ n₀ ≤ friends.card ∧ ∃ c : fin 2, ∀ e ∈ friends, f e = c,
+  {
+    cases finset.exists_le_card_fiber_of_mul_le_card_to_type f xfcn with y hy,
+    use finset.filter (λ (x : edge), f x = y) xfmin,
+    refine ⟨_, hy, _⟩, exact finset.filter_subset (λ (x : edge), f x = y) xfmin,
+    use y, intros e emem, exact (finset.mem_filter.1 emem).2,
+  },
+  rcases pigeonhole with ⟨friends, fss, fcard, ⟨color, fcolor ⟩ ⟩,
+  set friendvs:= finset.image (λ x : edge, x 1) friends,
+  have vinj: set.inj_on (λ (x : edge), x 1) friends, {
+    intros x xf y yf xyv, ext, cases mem_fin_2 x_1,
+    have xfm: x ∈ xfmin, apply fss xf, have yfm: y ∈ xfmin, apply fss yf,
+    rw h, simp at xfm yfm, simp [xfm, yfm], rw h, dsimp at xyv, rw xyv,
+  },
+  have fvcard : friendvs.card = friends.card := finset.card_image_of_inj_on vinj, rw ← fvcard at fcard,
+  rcases hn₀ friendvs fcard f with ⟨y, yss, yc, yf⟩,
+  have fvx: friendvs ⊆ X,
+  {
+    intros v vfvs, rw finset.mem_image at vfvs, rcases vfvs with ⟨a, af, ha⟩, 
+    have a1x:= (set.mem_to_finset.1 (fss af)).2, rw ha at a1x, exact a1x,
+  },
+  have xmny: xmin ∉ y, 
+  {
+    intro xmy, rcases (finset.mem_image.1 (yss xmy)) with ⟨a, af, a1⟩, 
+    have a0:=(set.mem_to_finset.1 (fss af)).1, rw ← a1 at a0, 
+    apply absurd (a.inj' a0) zero_ne_one, 
+  },
+  set Z:= finset.cons xmin y xmny, use Z, refine ⟨_, _, _⟩,
+  {
+    refine finset.cons_subset.2 ⟨_, _⟩, exact finset.min'_mem X xnone,
+    exact finset.subset.trans yss fvx, 
+  },
+  {
+    rw finset.card_cons xmny, rw yc, 
+  },
+  {
+    intros vertex vz, cases finset.mem_cons.1 vz, use color, intros e efz, rw h at efz, 
+    apply fcolor e, cases (set.mem_to_finset.1 efz) with e0m e1z, cases finset.mem_cons.1 e1z with e1m e1y, 
+    rw ← e0m at e1m, apply absurd (e.inj' e1m).symm zero_ne_one,
+    rcases (finset.mem_image.1 (yss e1y)) with ⟨a, af, a1⟩, have aeqe: a = e, 
+    { ext, cases mem_fin_2 x, rw h_1, rw (set.mem_to_finset.1 (fss af)).1,
+      exact e0m.symm, rw h_1, exact a1,
+    },
+    rw ← aeqe, exact af,
+    cases yf vertex h with c hc, use c, intros e efz, cases (set.mem_to_finset.1 efz) with e0m e1z,
+    cases finset.mem_cons.1 e1z with e1m e1y, have e0x: e 0 ∈ X, rw e0m, exact fvx (yss h),
+    have e1le0: e 1 ≤ e 0, rw e1m, apply finset.min'_le X (e 0) e0x, have:= e.map_rel_iff'.1 e1le0, 
+    norm_num at this, have eefy: e ∈ edges_from_finset y vertex, apply set.mem_to_finset.2, 
+    use [e0m, e1y], exact hc e eefy, 
+  },
 end
 
 
@@ -187,66 +263,12 @@ end
 #check fintype.exists_le_card_fiber_of_mul_le_card
 #check finset.order_emb_of_fin
 
-def has_favorite_color (n : ℕ) (v : fin n) (f : E n → fin 2) :=
-∃ c: fin 2, ∀ (e ∈ edges_from n v), f e = c
-
-lemma favcolor_procession (k : ℕ): 
-∃ n₀: ℕ, ∀ n, n₀ ≤ n → ∀ f: E n → fin 2,
-∃ s : (fin (k) ↪o fin n), ∀ v:fin (k), has_favorite_color n (s v) f:=
-begin
-    induction k with k ih, {use 0, intros n ng f, refine ⟨⟨⟨fin_zero_elim, fin_zero_elim⟩, fin_zero_elim⟩, fin_zero_elim⟩},
-    {
-      
-      cases ih with n₀ hn₀, use 2*n₀+1, intros n nge2 f, 
-      
-      cases n, linarith, 
-            
-      set f0 := edges_from (n+1) ⊥ with f0_def, 
-      have f0c: (fintype.card(fin 2))*n₀ ≤ f0.to_finset.card, sorry, --figure out how to represent f0 cardinality
-      have pigeonhole: 
-        ∃ friends : (finset (E (n+1))), friends ⊆ f0.to_finset ∧ n₀ ≤ finset.card friends ∧ ∃ c : fin 2, ∀ e ∈ friends, f e = c, 
-      { 
-        cases finset.exists_le_card_fiber_of_mul_le_card_to_type f f0c with y hy,
-        use (finset.filter (λ (x: E n.succ), f x = y) f0.to_finset),
-        refine ⟨_, hy, _⟩, simp, use y, intros e emem, exact (finset.mem_filter.1 emem).2, 
-      }, 
-      rcases pigeonhole with ⟨friends, fss, ⟨fcard, color, fcolor⟩⟩,
-      have fclens: friends.card ≤ n.succ, sorry, --f0 card issue again
-      set f_friends: E friends.card → fin 2:=λ x, f (edge_upconvert x fclens),
-      cases hn₀ friends.card fcard f_friends with s₀ hs₀, --gets uncertain after this
-      set friendvs := finset.image (λ (x : E n.succ), x 1) friends, 
-      have vinj: set.inj_on (λ (x : E n.succ), x 1) friends,
-      {
-        intros x xf y yf xyv, ext, cases mem_fin_2 x_1, rw h, have xf0: x ∈ f0,
-        apply set.mem_to_finset.1 (fss xf), have yf0: y ∈ f0,
-        apply set.mem_to_finset.1 (fss yf), simp at xf0 yf0, simp [yf0, xf0], 
-        rw h, dsimp at xyv, rw xyv, 
-      }, 
-      have fvemb:= finset.order_emb_of_fin friendvs (finset.card_image_of_inj_on vinj), 
-      have proc_in_friends:= order_embedding.comp s₀ fvemb, 
-    },
-end
-
 theorem finramsey: 
-∀ k: ℕ, 2 ≤ k → ∃ n₀: ℕ, ∀ n, n₀ ≤ n → 
-∀ f:E n → fin 2,
-∃ s: (fin k ↪o fin n),
-∃ c : fin 2, ∀ e:E k, f (e.comp s)=c :=
+∀ k: ℕ,  ∃ n₀: ℕ, ∀ n : finset ℕ, n₀ ≤ n.card → 
+∀ f: edge → fin 2,
+∃ s ⊆ n,
+∃ c : fin 2, ∀ v : s, ∀ e : edges_from_finset s v, f e = c:=
 begin
-  intros k kg2, induction k with k hk, norm_num at kg2, cases kg2 with ke2 kg2,
-  { --valiant effort but wrong strategy
-    use 2, intros n ng2 f, let s: fin 2 ↪o fin n := 
-    {to_fun := λ x, ⟨x.val, lt_of_lt_of_le x.property ng2⟩,
-    inj' := 
-    begin
-      intros a b h, dsimp at h, apply fin.eq_of_veq, apply fin.veq_of_eq h,
-    end,
-    map_rel_iff' := 
-    begin
-      intros a b, dsimp, refl,
-    end},
-    use s, use f s, intro e, congr, simp, have h: e=order_embedding.refl, {
-      apply subsingleton.elim (_ : fin 2 ↪o fin 2) _, 
-    }, rw h, simp,
-  }, change 2 ≤ k at kg2, cases hk kg2 with n₀ hn₀, 
+  intro k, cases favcolor_fixed (2*k) with n₀ hn₀, use n₀, intros n ncard f, 
+  rcases hn₀ n ncard f with ⟨y, ysubn, yc, yfav⟩, set ytoc: y → fin 2:= 
 end
